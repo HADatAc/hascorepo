0508c8a0a0118377c71af9b06a165b0f83579136
Tiago Moreira <tiago.moreira@ipiaget.pt>
2024-10-18 17:49:16 +0100

    Fix Drupal Installation Script and Configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -e
 
-# Esperar pelo banco de dados estar disponível
+# Wait for the database to be available
 until nc -z -v -w30 $DB_HOST 3306
 do
-  echo "Aguardando conexão com o banco de dados..."
+  echo "Waiting for database connection..."
   sleep 5
 done
-echo "Banco de dados disponível - executando comando"
+echo "Database available - executing command"
 
-# Definir o diretório root do Drupal
-export DRUPAL_ROOT="/var/www/html/web"
+# Set the root directory of Drupal
+export DRUPAL_ROOT="/opt/drupal"
 DRUSH_COMMAND="drush --root=${DRUPAL_ROOT}"
 
 APACHE_PORT_CONF="/etc/apache2/ports.conf"
 echo "" > $APACHE_PORT_CONF
 echo "Listen 8081" >> $APACHE_PORT_CONF
-# Criar o arquivo de configuração do Apache
+# Create the Apache configuration file
 APACHE_SITE_CONF="/etc/apache2/sites-available/000-default.conf"
 if ! grep -q "VirtualHost \*:8081" $APACHE_SITE_CONF; then
   echo "<VirtualHost *:8081>
-      DocumentRoot ${DRUPAL_ROOT}
-      <Directory ${DRUPAL_ROOT}>
+      DocumentRoot ${DRUPAL_ROOT}/web
+      <Directory ${DRUPAL_ROOT}/web>
           Options Indexes FollowSymLinks
           AllowOverride All
           Require all granted
@@ -33,21 +33,36 @@ fi
 
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
-# Ativar o site
+# Enable the site
 a2ensite 000-default
 
-# Verificar se o settings.php já está configurado
-if [ ! -f "$DRUPAL_ROOT/sites/default/settings.php" ]; then
-    echo "Criando settings.php e services.yml..."
-    cp $DRUPAL_ROOT/sites/default/default.settings.php $DRUPAL_ROOT/sites/default/settings.php
-    cp $DRUPAL_ROOT/sites/default/default.services.yml $DRUPAL_ROOT/sites/default/services.yml
-    chmod 644 $DRUPAL_ROOT/sites/default/settings.php
-    chmod 644 $DRUPAL_ROOT/sites/default/services.yml
-    chown www-data:www-data $DRUPAL_ROOT/sites/default/settings.php
-    chown www-data:www-data $DRUPAL_ROOT/sites/default/services.yml
+# Check if settings.php is already configured
+if [ ! -f "$DRUPAL_ROOT/web/sites/default/settings.php" ]; then
+    echo "Creating settings.php and services.yml..."
+    
+    # Check if default.settings.php and default.services.yml exist
+    if [ -f "$DRUPAL_ROOT/web/sites/default/default.settings.php" ]; then
+        cp $DRUPAL_ROOT/web/sites/default/default.settings.php $DRUPAL_ROOT/web/sites/default/settings.php
+        echo "settings.php created from default.settings.php"
+    else
+        echo "default.settings.php not found. Make sure the Drupal installation is complete."
+        exit 1
+    fi
+    
+    if [ -f "$DRUPAL_ROOT/web/sites/default/default.services.yml" ]; then
+        cp $DRUPAL_ROOT/web/sites/default/default.services.yml $DRUPAL_ROOT/web/sites/default/services.yml
+        echo "services.yml created from default.services.yml"
+    else
+        echo "default.services.yml not found. Make sure the Drupal installation is complete."
+        exit 1
+    fi
+    
+    chmod 644 $DRUPAL_ROOT/web/sites/default/settings.php
+    chmod 644 $DRUPAL_ROOT/web/sites/default/services.yml
+    chown www-data:www-data $DRUPAL_ROOT/web/sites/default/settings.php
+    chown www-data:www-data $DRUPAL_ROOT/web/sites/default/services.yml
 fi
-
-# Configurar as credenciais do banco de dados no settings.php
+# Configure the database credentials in settings.php
 sed -i "s/\$databases = \[\];/\$databases['default']['default'] = array( \
   'database' => '${DB_NAME}', \
   'username' => '${DB_USER}', \
@@ -55,40 +70,40 @@ sed -i "s/\$databases = \[\];/\$databases['default']['default'] = array( \
   'host' => '${DB_HOST}', \
   'driver' => 'mysql', \
   'prefix' => '', \
-);/" $DRUPAL_ROOT/sites/default/settings.php
+);/" $DRUPAL_ROOT/web/sites/default/settings.php
 
-# Verificar se o site Drupal já está instalado
+# Check if the Drupal site is already installed
 if ! $DRUSH_COMMAND status bootstrap | grep -q 'Successful'; then
-    echo "Instalando o site Drupal..."
+    echo "Installing Drupal site..."
     $DRUSH_COMMAND site:install standard \
         --db-url=mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME} \
         --site-name="Drupal Site" --account-name=admin --account-pass=admin -y
 else
-    echo "Drupal já está instalado."
+    echo "Drupal is already installed."
 fi
 
-echo "Habilitando hasco_barrio..."
+echo "Enabling hasco_barrio..."
 $DRUSH_COMMAND -v theme:enable hasco_barrio -y
 
 if $DRUSH_COMMAND pml --type=theme --status=enabled | grep -q 'hasco_barrio'; then
     $DRUSH_COMMAND config-set system.theme default hasco_barrio -y
-    echo "Tema hasco_barrio definido como padrão com sucesso."
+    echo "hasco_barrio theme set as default successfully."
 else
-    echo "Erro ao habilitar o tema hasco_barrio."
+    echo "Error enabling hasco_barrio theme."
 fi
 
-# Limpar cache novamente após definir o tema
+# Clear cache again after setting the theme
 $DRUSH_COMMAND cr
 
-echo "Habilitando módulos..."
-MODULES=("color" "key" "devel")
+echo "Enabling modules..."
+MODULES=("color" "key" "rep" "sir" "devel")
 for MODULE in "${MODULES[@]}"; do
-    echo "Habilitando módulo: $MODULE"
-    $DRUSH_COMMAND en $MODULE -y || echo "Erro ao habilitar módulo: $MODULE"
+    echo "Enabling module: $MODULE"
+    $DRUSH_COMMAND en $MODULE -y || echo "Error enabling module: $MODULE"
 done
 
-# Limpar cache
+# Clear cache
 $DRUSH_COMMAND cr
 
-# Iniciar o Apache em foreground
+# Start Apache in foreground
 apache2-foreground
