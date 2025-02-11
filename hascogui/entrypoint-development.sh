a914ee7b4bbd2272342e546e1a4340ac6f6221f4
Tiago Moreira <tiago.moreira@ipiaget.pt>
2025-02-11 19:49:16 +0000

    Graxiom

--- hascogui/entrypoint-development.sh
+++ hascogui/entrypoint-development.sh
@@ -120,7 +120,7 @@ fi
 $DRUSH_COMMAND cr
 
 echo "Enabling modules..."
-MODULES=("color" "key" "rep" "sir" "std" "sem" "dpl" "devel")
+MODULES=("color" "key" "devel")
 for MODULE in "${MODULES[@]}"; do
     echo "Enabling module: $MODULE"
     $DRUSH_COMMAND en $MODULE -y || echo "Error enabling module: $MODULE"
