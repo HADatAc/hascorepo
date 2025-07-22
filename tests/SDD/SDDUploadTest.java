fd2cb48b465f0c177313eec75d05457fbe373fab
Kael <41486985+JyanseiKasch@users.noreply.github.com>
2025-07-22 13:19:43 +0100

    Alteração parametrização e otimização de código

--- tests/SDD/SDDUploadTest.java
+++ tests/SDD/SDDUploadTest.java
@@ -9,17 +9,34 @@ public class SDDUploadTest extends BaseUpload {
 
     private final String sddType = System.getProperty("sddType", "DPQ"); // default to DPQ
 
+
+
     @Test
     @DisplayName("Upload a valid SDD file with type DPQ or DEMO")
-    void shouldUploadSDDFileSuccessfully() throws InterruptedException {
+      void shouldUploadSDDFileSuccessfully() throws InterruptedException {
+        switch(sddType){
+            case "DPQ", "DEMO" :
         navigateToUploadPage("sdd");
-
         fillInputByLabel("Name", "testeSDD" + sddType);
         fillInputByLabel("Version", "1");
-
-        File file = new File("tests/testfiles/SDD-NHANES-2017-2018-" + sddType + ".xlsx");
-        uploadFile(file);
-
+        File filenhanes = new File("tests/testfiles/SDD-NHANES-2017-2018-" + sddType + ".xlsx");
+        uploadFile(filenhanes);
         submitFormAndVerifySuccess();
+        break;
+        case "WS" :
+            navigateToUploadPage("sdd");
+
+            fillInputByLabel("Name", "testeSDD");
+            fillInputByLabel("Version", "1");
+
+            File filews = new File("tests/testfiles/SDD-WS.xlsx");;
+            uploadFile(filews);
+
+            submitFormAndVerifySuccess();
+            break;
+        default:
+                throw new IllegalArgumentException("Invalid SDD type: " + sddType);
+        }
     }
+
 }
