1157ff9dd4438c8c3ab7cf3d5abc240288d3117a
Kael <41486985+JyanseiKasch@users.noreply.github.com>
2025-08-11 14:15:53 +0100

    Maven Added scripts added and moved tests to src/test/java/tests

---
+++ src/test/java/tests/utils/FullIngestWSTestDRAFT.java
@@ -0,0 +1,51 @@
+package tests.utils;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.*;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+
+import  tests.DA.DAIngestTest;
+import tests.DP2.DP2IngestTest;
+import tests.DSG.DSGIngestTest;
+import tests.INS.INSFullIngest;
+import tests.SDD.SDDIngestDPQTest;
+import tests.STR.STRIngestTest;
+import tests.base.BaseIngest;
+
+public class FullIngestWSTestDRAFT {
+
+    private final Launcher launcher = LauncherFactory.create();
+
+    @BeforeAll
+    static void setMode() {
+        BaseIngest.ingestMode = "draft";
+    }
+
+    @Test
+    void runOnlyIngestsForCurrentMode() throws InterruptedException {
+
+
+        // SDD
+        runTestClass(SDDIngestDPQTest.class);
+        Thread.sleep(5000);
+
+        // DSG
+        runTestClass(DSGIngestTest.class);
+        Thread.sleep(5000);
+
+
+    }
+
+    private void runTestClass(Class<?> testClass) {
+        System.out.println("===> Running: " + testClass.getSimpleName());
+
+        launcher.execute(
+            LauncherDiscoveryRequestBuilder.request()
+                .selectors(DiscoverySelectors.selectClass(testClass))
+                .build()
+        );
+    }
+}
